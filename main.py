c059ff7d475b469afadc344d8d0ae8c6b5fa84eb
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-04-19 20:08:19 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,21 @@
+from itertools import zip_longest
+
+def zip_longest_list(list1, list2):
+    return list(zip_longest(list1, list2))
+
+list1 = [1, 2, 3]
+list2 = ['a', 'b']
+
+print(zip_longest_list(list1, list2))
+```
+
+```python
+from itertools import zip_longest
+
+def zip_longest_list(list1, list2):
+    return list(zip_longest(list1, list2))
+
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c', 'd']
+
+print(zip_longest_list(list1, list2))
